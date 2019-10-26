9161a1bfcbce439d6d814c2b7f90df74ad70e6ff
Amalina Nusyirwan <lina.webdev@gmail.com>
2019-10-26 14:14:28 +0800

    Made small changes n add country file in helper

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://mage22.karaksacode.com/srp/srp/';
+$config['base_url'] = 'https://mage22.karaksacode.com/srp_ori/srp/';
 
 /*
 |--------------------------------------------------------------------------
