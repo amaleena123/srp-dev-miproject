f8a138d01fbc68412debe9c49dc673579edb762e
hilmigit <hilmi.jamal@gmail.com>
2019-10-30 00:24:13 +0800

    Update Ejen_model.php
    update: function get_all_ejens. select * records except ejen_status = 3 = rekod dihapuskan. function delete. to update ejen_status = 3 by ejen_id

--- application/models/Ejen_model.php
+++ application/models/Ejen_model.php
@@ -1,47 +1,47 @@
 <?php 
 defined('BASEPATH') OR exit('no direct script access allowed');
 
-class Ejen_model extends CI_Model 
-{
+class Ejen_model extends CI_Model {
 
-	public function __construct() {
-
-		parent::__construct();
+    public function __construct() {
+        parent::__construct();
         $this->load->database();
         $this->prefix_field = "ejen_";
-	}
+    }
 
-	public function get_all_ejens() {
-		$this->db->from('ma_ejen');
+    public function get_all_ejens() { //where not in status = 3
+        $status = 3;
+
+        $this->db->where_not_in('ejen_status', $status);
+        $this->db->from('ma_ejen');
         $query=$this->db->get();
         return $query->result();
-	}
+    }
 
-	public function get_by_id($id) {
+    public function get_by_id($id) {
         $this->db->from('ma_ejen');
         $this->db->where('ejen_id',$id);
         $query = $this->db->get();
-  
         return $query->row();
     }
-	
-	public function create($data) {
-         
+    
+    public function create($data) {
         $this->db->insert('ma_ejen', $this->add_prefix_fieldname($data));
         return $this->db->insert_id();
-       
     }
- 
+
     public function update($data) {
         $where = array('ejen_id' => $this->input->post('ejen_id'));
-         $this->db->update('ma_ejen', $this->add_prefix_fieldname($data), $where);
-         return $this->db->affected_rows();
+        $this->db->update('ma_ejen', $this->add_prefix_fieldname($data), $where);
+        return $this->db->affected_rows();
     }
-  
-    public function delete() {
+
+    public function delete($status) {
         $id = $this->input->post('ejen_id');
+        $status = $this->input->post('ejen_status');
+
         $this->db->where('ejen_id', $id);
-        $this->db->delete('ma_ejen');
+        $this->db->update('ma_ejen', $this->add_prefix_fieldname($status));
     }
 
     public function add_prefix_fieldname($data) {
